82cf874c2f500df845bf0539b713a47954abb72e
Steve <Stevekivaamwanzia@gmail.com>
2024-03-14 01:20:58 +0300

    project component and contact updates

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import github from '../assets/github.png'
 import linkedin from '../assets/linkedin.png'
@@ -8,24 +8,37 @@ import ovals from '../assets/Navovals.png'
 
 const Contact = () => {
     const form = useRef();
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [message,setMessage] = useState("")
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setName("");
+    //     setEmail("");
+    //     setMessage("");
+    // }
 
-    emailjs
-      .sendForm('service_nk3rjop', 'template_hs26atq', form.current, {
-        publicKey: 'abu5ydmXD-6J6hJRe',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-          console.log('Message Sent!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm('service_nk3rjop', 'template_hs26atq', form.current, {
+            publicKey: 'abu5ydmXD-6J6hJRe',
+        })
+        .then(
+            () => {
+            console.log('SUCCESS!');
+            alert('Message Sent!');
+            },
+            (error) => {
+            console.log('FAILED...', error.text);
+            },
+        );
+        setName("");
+        setEmail("");
+        setMessage("");
+    };
   return (
     <div className='font-SpaceGrotesk bg-[#242424] text-white w-full flex flex-col gap-6 pt-16 px-4 md:px-6 relative md:justify-center md:items-center lg:px-20' >
         <img className=' absolute top-[280px] top- left-0 h-[126px] ' src={ovals} alt="" />
@@ -35,9 +48,9 @@ const Contact = () => {
                 <p className=' text-xs text-[#D9D9D9] text-center md:text-[18px] leading-[28px] lg:text-left lg:text-[16px]'>I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
             </div>
             <form ref={form} onSubmit={sendEmail} className=' flex flex-col gap-4 border-b-[1px] border-solid border-white pb-16 md:w-[400px] md:border-none '>
-                <input className=' bg-[#242424] text-xs md:text-sm border-b-[1px] border-solid px-5 pb-4 pt-2  border-white outline-none' type="text " placeholder='NAME' name="user_name"/>
-                <input className=' bg-[#242424] text-xs md:text-sm border-b-[1px] border-solid px-5 pb-4 pt-2 border-white outline-none' type="email" placeholder='EMAIL' name="user_email" required />
-                <textarea className=' bg-[#242424] bg-transparent text-xs md:text-sm border-b-[1px] border-solid px-5 pt-2 border-white outline-none' placeholder='MESSAGE' cols="30" rows="5" name="message"></textarea>
+                <input className=' bg-[#242424] text-xs md:text-sm border-b-[1px] border-solid px-5 pb-4 pt-2  border-white outline-none' type="text " placeholder='NAME' name="user_name" value={name} onChange={(e)=> setName(e.target.value)}/>
+                <input className=' bg-[#242424] text-xs md:text-sm border-b-[1px] border-solid px-5 pb-4 pt-2 border-white outline-none' type="email" placeholder='EMAIL' name="user_email" required value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <textarea className=' bg-[#242424] bg-transparent text-xs md:text-sm border-b-[1px] border-solid px-5 pt-2 border-white outline-none' placeholder='MESSAGE' cols="30" rows="5" name="message" value={message} onChange={(e)=> setMessage(e.target.value)}></textarea>
                 <input type="submit" value="SEND MESSAGE" className=' hover:cursor-pointer pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm md:text-[16px] self-end pt-3'/>
             </form>
         </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import todoImg from '../assets/todoimg.png'
 import country from '../assets/countrysearch.png'
 import dev from '../assets/devfinder.png'
 import audiophile from '../assets/audiophile.png'
 
 const Projects = () => {
+
+  const [hover, setHover] = useState(false)
+
+  const handleHover = () => {
+    setHover((prev) => !prev);
+    console.log(hover);
+  }
+
+  useEffect(() => {
+    handleHover
+  })
+
   return (
     <div className='font-SpaceGrotesk bg-black text-white min-h-screen w-full flex flex-col gap-6 pt-16 px-4 pb-16 md:px-6 lg:px-20'>
         <div className=' flex justify-between items-center'>
             <h2 className=' text-[28px] font-bold md:text-[72px]'>Projects</h2>
             <p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm md:text-[16px] hover:cursor-pointer hover:text-[#4EE1A0]'>CONTACT ME</p>
         </div>
-        <div className='flex flex-col md:grid md:grid-cols-2'>
+        <div className='flex flex-col md:grid md:grid-cols-2 gap-6'>
           <div className=' flex flex-col gap-5 '>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={country} alt="Todo"/>
+            <div className=' lg:relative lg:w-[450px] lg:h-[350px] cursor-pointer' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={country} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://country-search-steve.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="https://github.com/SteveMwanzia/Countries_app_project" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
+              
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>COUNTRY SEARCH</h4>
@@ -31,8 +50,14 @@ const Projects = () => {
           </div>
 
           <div className=' flex flex-col gap-5'>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={dev} alt="Todo"/>
+          <div className=' lg:relative lg:w-[450px] lg:h-[350px]' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={dev} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://github-search-module-project-steve.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="https://github.com/SteveMwanzia/GitHub-Search-Module-Project-2" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>DEV FINDER APP</h4>
@@ -49,8 +74,14 @@ const Projects = () => {
           </div>
 
           <div className=' flex flex-col gap-5'>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={audiophile} alt="Todo"/>
+          <div className=' lg:relative lg:w-[450px] lg:h-[350px]' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={audiophile} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://audiophile-ecom-stevekm.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="https://github.com/SteveMwanzia/audiophile" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>E-COMMERCE WEBSITE</h4>
@@ -67,8 +98,14 @@ const Projects = () => {
           </div>
 
           <div className=' flex flex-col gap-5'>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={todoImg} alt="Todo"/>
+          <div className=' lg:relative lg:w-[450px] lg:h-[350px]' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={country} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://country-search-steve.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="https://github.com/SteveMwanzia/Todo-Module-Project" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>TODO WEB APP</h4>
@@ -85,8 +122,14 @@ const Projects = () => {
           </div>
 
           <div className=' flex flex-col gap-5'>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={todoImg} alt="Todo"/>
+          <div className=' lg:relative lg:w-[450px] lg:h-[350px]' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={country} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://country-search-steve.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>TODO WEB APP</h4>
@@ -103,8 +146,14 @@ const Projects = () => {
           </div>
 
           <div className=' flex flex-col gap-5'>
-            <div>
-              <img className=' w-[340px] h-[250px]' src={todoImg} alt="Todo"/>
+          <div className=' lg:relative lg:w-[450px] lg:h-[350px]' onClick={handleHover}>
+              <img className=' w-[340px] h-[250px] lg:w-[450px] lg:h-[350px]' src={country} alt="Todo"/>
+              {hover ? (
+                <div className=' lg:flex lg:gap-12 hidden lg:absolute lg:top-0 lg:w-full lg:h-full lg:bg-black lg:opacity-75'>
+                <a href="https://country-search-steve.netlify.app" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-28 lg:left-44 lg:absolute'>VIEW PROJECT</p></a>
+                <a href="" target="_blank"><p className=' pb-1 border-b-2 border-solid border-[#4EE1A0] tracking-widest text-sm hover:cursor-pointer hover:text-[#4EE1A0] lg:top-1/2 lg:left-48 lg:absolute'>VIEW CODE</p></a>
+              </div>
+              ) :null}
             </div>
             <div>
               <h4 className=' text-[18px] font-semibold'>TODO WEB APP</h4>
